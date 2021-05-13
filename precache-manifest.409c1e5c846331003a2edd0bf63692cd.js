0b5fd4bc6a287325474b9d856de217ee5ea8c5c3
Femi <ologunwafemi@gmail.com>
2021-05-13 01:27:09 +0100

    Updates

---
+++ precache-manifest.409c1e5c846331003a2edd0bf63692cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cefd08e8d05936131fb72e9f0fa5bed",
+    "url": "/-1-birthday-reminder/index.html"
+  },
+  {
+    "revision": "919825b1b239edc513f7",
+    "url": "/-1-birthday-reminder/static/css/main.5f6885b7.chunk.css"
+  },
+  {
+    "revision": "3bdc1d8518940277ff32",
+    "url": "/-1-birthday-reminder/static/js/2.ab0cd556.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/-1-birthday-reminder/static/js/2.ab0cd556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919825b1b239edc513f7",
+    "url": "/-1-birthday-reminder/static/js/main.fe7d05a9.chunk.js"
+  },
+  {
+    "revision": "92e8b1c20ff64ae02d7a",
+    "url": "/-1-birthday-reminder/static/js/runtime-main.9d351467.js"
+  }
+]);
